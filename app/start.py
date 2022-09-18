edcb427d7c0d5581004b1add9a853aa7a2a68f98
Jeroen Op de Beek <59441635+Jeroenodb@users.noreply.github.com>
2022-09-18 10:14:24 +0200

    main,py added

--- app/start.py
+++ app/start.py
@@ -1,4 +1,16 @@
-import time, app.secrets as secrets
+import time, app.secrets as secrets, network
+from mqtt import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+import gc
+import ntptime
+import json
+from neopixel import NeoPixel
+from machine import Pin
+
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
@@ -9,61 +21,66 @@ def do_connect():
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
-    
-do_connect()
 
+# initialization
+esp.osdebug(None)
+gc.collect()
+do_connect()
+ntptime.settime()
+np=NeoPixel(Pin(21),3)
+
+sta_if = network.WLAN(network.STA_IF)
 def show_time(tns):
     ts=time.localtime(int(tns//1000000000))
     tus=(tns%1000000000)/1000
     print(ts,tus)
     
-    
-import ntptime
-ntptime.settime()
-
-
-    
-    
-from mqtt import MQTTClient
-import ubinascii
-import machine
-import micropython
-import network
-import esp
-esp.osdebug(None)
-import gc
-gc.collect()
-
-ssid = 'REPLACE_WITH_YOUR_SSID'
-password = 'REPLACE_WITH_YOUR_PASSWORD'
-mqtt_server = '192.168.2.5'
-#EXAMPLE IP ADDRESS
-#mqtt_server = '192.168.1.144'
+mqtt_server = secrets.MQTT_SERVER
+# unique client ID based on mac address
 client_id = ubinascii.hexlify(machine.unique_id())
-topic_sub = b'robot/notification'
-topic_pub = b'robot/time/'+client_id
+topic_cmd_sub = b'robot/'+client_id+b'/cmd'
+topic_pub = b'robot/'+client_id
 
 last_message = 0
 message_interval = 5
 counter = 0
 
+# cmds
+# "status"
+# "network"
+# {'neopixel':{"nr":1,"rgb":[10,0,0]}}
 
 
 def sub_cb(topic, msg):
   print((topic, msg))
-  if topic == b'robot/notification' and msg == b'ntp':
-    print('ESP received hello message')
-    ntptime.settime()
-  if topic == b'robot/notification' and msg == b'time':
-    client.publish(topic_pub+b'/time', ("%d"%((time.time_ns()%1000000000)//1000000)).encode('utf-8'))
+  js_msg=None
+  try:
+      js_msg=json.loads(msg)
+      print('received js_msg',js_msg)
+  except ValueError as err:
+      print("ValueError: {0}".format(err))
+      # <TO DO> send a status report back to MQTT server
+  if topic == topic_cmd_sub:
+      if js_msg == 'status':
+          client.publish(topic_pub+b'/battery', ("%d perc"%95).encode('utf-8'))
+      elif js_msg == 'network':
+          if sta_if.isconnected():
+              client.publish(topic_pub+b'/network', (str(sta_if.ifconfig())).encode('utf-8'))
+      elif 'neopixel' in js_msg:
+          neo=js_msg['neopixel']
+          np[neo['nr']]=neo['rgb']
+          np.write()
+          print("neopixel",neo["nr"],neo["rgb"])
+  #if topic == b'robot/notification' and msg == b'time':
+  #  client.publish(topic_pub+b'/time', ("%d"%((time.time_ns()%1000000000)//1000000)).encode('utf-8'))
 
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub
   client = MQTTClient(client_id, mqtt_server)
   client.set_callback(sub_cb)
   client.connect()
-  client.subscribe(topic_sub)
-  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  client.subscribe(topic_cmd_sub)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_cmd_sub))
   return client
 
 def restart_and_reconnect():
@@ -81,7 +98,7 @@ while True:
     client.check_msg()
     if (time.time() - last_message) > message_interval:
       msg = b'Hello #%d' % counter
-      client.publish(topic_pub, msg)
+      client.publish(topic_pub+b'/counter/', msg)
       last_message = time.time()
       counter += 1
   except OSError as e:
